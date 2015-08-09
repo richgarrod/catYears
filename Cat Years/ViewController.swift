a1e8b79b6b63598f6d04ede75b2d3fb1a6dc4148
richgarrod <=>
2015-08-09 12:11:16 +0100

    Working cat app with some non working code

--- Cat Years/ViewController.swift
+++ Cat Years/ViewController.swift
@@ -9,7 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var outputField: UILabel!
+    @IBOutlet var outputLabel: UILabel!
+    @IBOutlet var catAge: UITextField!
+    
+    @IBAction func catAgeChanged(sender: AnyObject) {
+        changeLabel()
+    }
+    
+    @IBAction func calcButton(sender: AnyObject) {
+        changeLabel()
+    }
+    
+    func changeLabel()
+    {
+        if catAge.text.isEmpty || catAge.text.toInt() == nil
+        {
+            return
+        }
+        
+        var enteredAge = catAge.text.toInt()!
+        var calculatedAge = enteredAge * 7
+        
+        outputLabel.text = "Your cat's age in cat years is \(calculatedAge)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
